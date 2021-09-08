07da088dbd5d1d50e9c360e9d95fc26200c0e682
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-08 10:33:00 +0530

    [03 React Basics & Working With Components] - 10 writing more complexe JSX code

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </>
   );
 }
 
