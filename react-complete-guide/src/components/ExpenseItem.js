c433e1c13dbcc83afd6a719016bdc351895e4f3d
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-08 12:25:25 +0530

    [03 React Basics & Working With Components] - 14 adding normal javascript logic to components

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./ExpenseItem.css";
-import moment from 'moment'
+// import moment from 'moment'
 
 function ExpenseItem(props) {
+    const year = props.date.getFullYear();
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
   return (
     <>
       <div className="expense-item">
-        <div>{moment(props.date).format("MMMM Do YYYY")}</div>
+        {/* <div>{moment(props.date).format("MMMM Do YYYY")}</div> */}
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
