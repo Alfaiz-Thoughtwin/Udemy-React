55828d6285ef29804a225f5594e881d41df7ce22
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-08 11:03:03 +0530

    [03 React Basics & Working With Components] - 11 adding basic CSS styling

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,11 +4,7 @@ import ExpenseItem from './components/ExpenseItem';
 function App() {
   return (
     <>
-      <div>March 28th 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
-      </div>
+      <ExpenseItem />
     </>
   );
 }
