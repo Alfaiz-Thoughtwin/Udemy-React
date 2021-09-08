c9fb90159f2eef79b6c0660eca7a026ba0b53173
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-08 12:15:19 +0530

    [03 React Basics & Working With Components] - 13 passing data via props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -2,18 +2,14 @@ import React from "react";
 import "./ExpenseItem.css";
 import moment from 'moment'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    // const expenseDate = new Date();
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294.67;
+function ExpenseItem(props) {
   return (
     <>
       <div className="expense-item">
-        <div>{moment(expenseDate).format("MMMM Do YYYY")}</div>
+        <div>{moment(props.date).format("MMMM Do YYYY")}</div>
         <div className="expense-item__description">
-          <h2>{expenseTitle}</h2>
-          <div className="expense-item__price">${expenseAmount}</div>
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
         </div>
       </div>
     </>
