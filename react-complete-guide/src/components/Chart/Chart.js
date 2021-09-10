f603079f7026337198e22bef18eaafd3d1aa6d90
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-10 12:30:17 +0530

    [05 Rendering Lists & Conditional Content] - 07 demo app adding a chart

---
+++ react-complete-guide/src/components/Chart/Chart.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+function Chart(props) {
+  return (
+    <>
+      <div className="chart">
+        {props.datapoints.map((dataPoint) => (
+          <ChartBar
+            key={dataPoint.id}
+            value={dataPoint.value}
+            maxValue={null}
+            label={dataPoint.label}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Chart;
---
+++ react-complete-guide/src/components/Chart/ChartBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./ChartBar.css";
+
+function ChartBar(props) {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default ChartBar;
