105c37b385593826374bec1c5c01d7e4df47030c
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-09-10 15:25:45 +0530

    [05 Rendering Lists & Conditional Content] - 09 wrap up & next steps

--- react-complete-guide/src/components/Chart/Chart.js
+++ react-complete-guide/src/components/Chart/Chart.js
@@ -3,14 +3,16 @@ import "./Chart.css";
 import ChartBar from "./ChartBar";
 
 function Chart(props) {
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValues);
   return (
     <>
       <div className="chart">
-        {props.datapoints.map((dataPoint) => (
+        {props.dataPoints.map((dataPoint) => (
           <ChartBar
             key={dataPoint.id}
             value={dataPoint.value}
-            maxValue={null}
+            maxValue={totalMaximum}
             label={dataPoint.label}
           />
         ))}
